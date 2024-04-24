b308eb31e362910e897a850d096183ba4ad8db4f
Jay Sanchez <jairo.sanchez01@utrgv.edu>
2024-04-23 23:04:09 -0500

    git commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import static functions.h.BiSearch;
+import static functions.h.Calculator;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        int num, num2, total;
+        String operation, result;
+        int results[] = {0};
+        Scanner keyboard = new Scanner(System.in);
+        try {
+            System.out.print("Enter a number: ");
+            num = keyboard.nextInt();
+            System.out.print("Enter an operation: ");
+            operation = keyboard.next();
+            System.out.print("Enter a number: ");
+            num2 = keyboard.nextInt();
+            total = Calculator(num,num2, operation);
+            System.out.println("The result is: " + total);
+            System.out.println("Do you want to store results?(Y/N): ");
+            result = keyboard.next();
+            if(result.equals("Y") || result.equals("y")){
+                System.out.println("Data Saved");
+            }else{
+                System.out.println("Data has been deleted");
+            }
+
+        } catch (Exception e) {
+            System.out.println("This is not a number.");
+        }
+    }
+}
---
+++ functions/h.java
@@ -0,0 +1,54 @@
+package functions;
+
+public class h {
+    public static boolean BiSearch(int[] arr, int value){
+        int start = 0;
+        int end = arr.length-1;
+        while (start <= end){
+            int mid = start + (end - start) /2;
+            if(arr[mid] == value){
+                return true;
+            }
+            if(arr[mid] < value){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+
+        return false;
+    }
+    public static int Calculator(int x, int y, String Operator){
+        int result;
+        if(Operator.equals("+")){
+            result = x + y;
+            return result;
+        }
+        if(Operator.equals("-")){
+            result = x-y;
+            return result;
+        }
+        if(Operator.equals("/")){
+            result = x / y;
+            return result;
+        }
+        if (Operator.equals("*")){
+            result = x * y;
+            return result;
+        }
+        else{
+            System.out.println("This is not a valid Option. Please try again");
+        }
+        return -1;
+    }
+
+    public static int[] AddResults(int[] results, int value){
+        int length = results.length+1;
+        int[] newArray = new int [length];
+        for(int c = 0; c < length; c++){
+
+        }
+        return newArray;
+    }
+}
